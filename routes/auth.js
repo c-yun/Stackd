b90432ef258d345d3fe0ad201bfe425cbb045454
Charles Yun <cy@Charless-MacBook-Pro.local>
2019-04-12 16:40:36 -0700

    Initial commit

---
+++ routes/auth.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+// Route for signup
+router.post('/signup', (req, res) => {
+    // See if the email is already in the db
+    User.findOne({email: req.body.email}, (err, user)=> {
+        // if yes, return an error
+        if (user) {
+            res.json({type: 'error', message: 'Email already exists'})
+        } else {
+            // if no, create the user in the db
+            let user = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            });
+            user.save((err, user) => {
+                if (err) {
+                    res.json({type: 'error', message: 'Database error creating user'})
+                } else {
+                    // sign a token (this is the login step)
+                    var token = jwt.sign(user.toObject(), process.env.JWT_SECRET, {
+                        expiresIn: '1d'
+                    });
+                    // return the token
+                    res.status(200).json({type: 'success', user: user.toObject(), token})
+                }
+            })
+        }
+    })
+});
+
+// Route for login
+router.post('/login', (req, res) => {
+    console.log('this is the submitted password', req.body.password)
+    // Find user in db
+    User.findOne({email: req.body.email}, (err, user) => {
+        if (!user) {
+            // if no user, return error
+            res.json({type: 'error', message: 'Account not found'})
+        } else {
+            // if user, check authentication
+            if (user.authenticated(req.body.password)) {
+                // if authenticated, sign a token (login)
+                var token = jwt.sign(user.toObject(), process.env.JWT_SECRET, {
+                    expiresIn: '1d'
+                });
+                // return the token
+                res.json({type: 'success', user: user.toObject(), token});
+            } else {
+                res.json({type: 'error', message: 'Authentication failure'});
+            }
+        }
+    })
+});
+
+// Route for token validation
+router.post('/me/from/token', (req, res) => {
+    // Make sure they sent us a token to check
+    let token = req.body.token;
+    if (!token) {
+        // if no token, return error
+        console.log('here')
+        res.json({type: 'error', message: 'You must pass a valid token!'});
+    } else {
+        // if token, verify it
+        jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+            if (err) {
+                // if invalid, return an error
+                console.log('there')
+                res.json({type: 'error', message: 'Invalid token. Please log in again.'});
+            } else {
+                // if token is valid...
+                // Look up the user in the db
+                User.findById(user._id, (err, user) => {
+                    if (err) {
+                        // if user doesn't exist, return an error
+                        console.log('other here')
+                        res.json({type: 'error', message: 'Database error during validation.'});
+                    } else {
+                        // if user exists, send user and token back to React
+                        res.json({type: 'success', user: user.toObject(), token});
+                    }
+                })
+            }
+        })
+    }
+});
+
+module.exports = router;
