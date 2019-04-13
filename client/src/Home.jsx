cba81062cedf70a71f7c930a824e28b17fd1e702
kenneth hong <kennethhong@kenneths-MacBook-Pro.local>
2019-04-13 15:28:55 -0700

    made home.jsx and schools.jsx

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -6,7 +6,12 @@ const Home = (props) => {
    return(
       <div className="homeBox">
          <h1> This Is Your Home Page</h1>
-      
+         <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Saepe expedita, optio atque quos sed dignissimos provident 
+            exercitationem minus assumenda nobis ad consequuntur officia rerum beatae praesentium 
+            deleniti corrupti facere cum.
+         </p>
       </div>
    )
 }
