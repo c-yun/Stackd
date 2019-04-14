3b855def3741a498bd35c0d87d84cc6a06b4318e
Charles Yun <cy@Charless-MacBook-Pro.local>
2019-04-14 15:44:14 -0700

    link header to app, added components css and font style to brand

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -5,7 +5,7 @@ const Header = (props) => {
     return (
         <div className='Header'>
             <Link to='/'>
-                <h1>Stackd</h1>
+                <h1 className='brand'>Stackd</h1>
             </Link>
         </div>
     )
