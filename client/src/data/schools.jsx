d773382688d72b07477e839817173f8290e5f6e8
Charles Yun <cy@Charless-MacBook-Pro.local>
2019-04-13 18:27:53 -0700

    school data

---
+++ client/src/data/schools.jsx
@@ -0,0 +1,67 @@
+
+const schools = [
+{
+    campus: 'GA',
+    cost: '$13,950 - $14,950',
+    len: '12 weeks',
+    loc: 'Atlanta, Austin, Boston, Chicago, Dallas, Denver, Los Angeles, New York City, Providence, San Diego, San Francisco, Seattle, Stamford, Toronto, Washington DC',
+    img: 'https://i.imgur.com/SmPccL2.png',
+    url: 'https://generalassemb.ly/'
+},
+{
+    campus: 'Coding Dojo',
+    cost: '$10,495 - $14,995',
+    len: '14 weeks',
+    loc: 'Chicago, Seattle, Silicon Valley, Los Angeles, Berkeley, Dallas, Tysons Corner, Tulsa, Boise',
+    img: '',
+    url: 'https://www.codingdojo.com/'
+},
+{
+    campus: 'Flatiron',
+    cost: '$15,000',
+    len: '14 weeks',
+    loc: 'New York, Houston, Atlanta, Seattle, Washington DC',
+    img: '',
+    url: 'https://flatironschool.com/'
+},
+{
+    campus: 'Galvanize',
+    cost: '$17,980',
+    len: '12 weeks',
+    loc: 'Seattle, San Francisco, Denver, Boulder, Austin, New York, Phoenix, London',
+    img: '',
+    url: 'https://www.galvanize.com/'
+},
+{
+    campus: 'App Academy',
+    cost: '$3,000 refundable deposit + $25,000 once you find a job',
+    len: '12 weeks',
+    loc: 'San Francisco, New York',
+    img: '',
+    url: 'https://www.appacademy.io/'
+},
+{
+    campus: 'Hack Reactor',
+    cost: '$17,980',
+    len: '13 weeks',
+    loc: 'San Francisco, Austin, Los Angeles, New York City',
+    img: '',
+    url: 'https://www.hackreactor.com/'
+},
+{
+    campus:'DevMountain',
+    cost: '$10,900 - $11,900',
+    len: '13 weeks',
+    loc: 'Provo, Phoenix, Dallas',
+    img: '',
+    url: 'https://devmountain.com/'
+},
+{
+    campus: 'Hackbright Academy',
+    cost: '$16,895',
+    len: '12 weeks',
+    loc: 'San Francisco, Oakland, San Jose',
+    img: '',
+    url: 'https://hackbrightacademy.com/'
+}
+]
