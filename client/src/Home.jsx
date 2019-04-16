0fe52d9233bb9662050f423763846165256f4fa5
kenneth hong <kennethhong@kenneths-MacBook-Pro.local>
2019-04-16 11:48:30 -0700

    saving local changes and layouts

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 // require('dotenv').config();
-import {Card, Button, Row, Col, Jumbotron, Container, CardGroup, CardDeck, Image} from "react-bootstrap"
+import {Card, Button, Row, Col, Jumbotron, Container, CardGroup, CardDeck, Image , Media} from "react-bootstrap"
 import axios from 'axios';
 import Vid from "./Images&Video/stacdVideo.mp4"
 import { faBookmark, faStar} from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+// import {Grid } from "semantic-ui-react"
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faBookmark } from '@fortawesome/free-solid-svg-icons';
 
@@ -90,25 +92,47 @@ render(){
    const news = this.state.news ?
    this.state.news.map((article, index) => (
       <div key={index} className="cardBox">
-      {/* <Container fluid> */}
-      {/* <Row  fluid className=""> */}
-      {/* <Container fluid> */}
-      {/* <Image className="imageBox" src={article.urlToImage} fluid /> */}
-      {/* </Container> */}
-      {/* </Row> */}
-      {/* </Container> */}
+      <Col>
+         <Card style={{width:"20em"}}>
+            <Card.Img variant="top" src={article.urlToImage} />
+               <Card.Body className="cardBody">
+                  <h3> {article.title}</h3>
+                  <p> {article.description}</p>
+                  <footer className="blockquote-footer">
+                  {article.author} 
+               </footer>
+               </Card.Body>
+               <Card.Body>
+                  <Row>
+                     <Col>
+                  <Card.Link href={article.url}><Button>Article</Button></Card.Link>
+                     </Col>
+                     <Col>
+                  <Button onClick={() => this.addToProfile(article)}> bookmark</Button>
+                     </Col>
+                  </Row>
+               </Card.Body>
+         </Card> 
+      </Col>
       </div>
    )): ""; 
-
+   
    return(
       <div className="homeBox">
-         {/* <Jumbotron fluid>
-            
-      </Jumbotron> */}
+         <Jumbotron fluid>
+            <video className="video-background" preload="true" muted="true" autoplay="true" loop="true"> 
+               <source src={Vid} type="video/mp4" />
+            </video> 
+         </Jumbotron> 
+   <br /> 
+      <div className="headerTopStories">
+         <h3 className="headerTopStory"> Top Trending Articles </h3>
+      </div>
    <br /> 
    <div className="newsBox">
-      <h3> Top Stories </h3>
-            {news}
+      <Row>
+         {news}
+      </Row>
    </div>
 </div>
    
@@ -121,16 +145,3 @@ export default Home;
 
 
 
-{/* <Card style={{width:"18em", flex:1}}>
-   <Card.Img variant="top" src={article.urlToImage} />
-      <Card.Body className="cardBody">
-         <Card.Title>{article.title}</Card.Title>
-         <Card.Text> {article.description} by {article.author}</Card.Text>
-         <Card.Link href={article.url}> Link </Card.Link> <br /> 
-         <Button onClick={() => this.addToProfile(article)}> {bookmarkIcon} </Button>
-   </Card.Body>
-</Card> */}
-
-{/* <video className="video-background" preload="true" muted="true" autoplay="true" loop="true"> */}
-               // <source src={Vid} type="video/mp4" />
-            {/* </video> */}
