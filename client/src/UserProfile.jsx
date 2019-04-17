fe1cd0b746f68d6b5fd6ce9bd856b655e716d4fd
kenneth hong <kennethhong@kenneths-MacBook-Pro.local>
2019-04-16 20:00:38 -0700

    start working on bio update route

--- client/src/UserProfile.jsx
+++ client/src/UserProfile.jsx
@@ -67,19 +67,7 @@ render(){
         </Card>
         </div>
     ))
-
-    // if (props.user) {
-        
-    //         <>
-    //             <div className='profile-img'>
-    //                 {(props.user.image && (
-    //                     <img src={props.user.image} alt='user' /> )) || (<img src='http://placekitten.com/g/200/200' alt='placeholder' />
-    //         )}
-    //         </div>
-    //         </>
-
-
-    let articles; 
+    // let articles; 
     if (this.state.articles.length){
         articles = this.state.articles.map((article, index) => (
             <div key={index} className="savedArticles" >
